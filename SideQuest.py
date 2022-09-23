841aa14111f7b4a1eba7eef64355817c9b859199
DTNevaeha <DTNevaeha@gmail.com>
2022-09-23 18:57:09 -0500

    Version 2.0 complete - more Deaths + Items

--- SideQuest.py
+++ SideQuest.py
@@ -65,7 +65,7 @@ room = {
         'west': 'Secret',
         'item': 'donut',
         'inspect': 'table',
-        'people': 'Gerald' + " and " + 'tony',
+        'people': 'Gerald' + " and " + 'Tony',
         'description': 'You can go south or east \n You hear nothing to the East \n You hear loud scratching noises to the South \n'
 
     },
@@ -73,7 +73,7 @@ room = {
     'Sun Room': {
         'north': 'Library',
         'east': 'Garden',
-        'inspect': 'dog',
+        'people': 'Dog',
         'item': 'fuel canister',
         'description': 'You can go north or east \n You hear the sound of someone shouting in the loudest whisper you ever heard to the north \n You see someone digging a deep hole to the East \n'
     },
@@ -83,8 +83,8 @@ room = {
         'west': 'Library',
         'south': 'Garden',
         'north': 'Dining Room',
-        'east': 'Bed Room',
-        'item': 'potion',
+        'east': 'Bedroom',
+        'people': 'Linda',
         'description': 'You can go North, South, East, West \n To the west you hear the sound of pages flipping \n Sounds of digging come from the south \n Loud chattering comes from the north \n An eerie silence to the east \n'
     },
 
@@ -94,51 +94,61 @@ room = {
         'west': 'Sun Room',
         'south': 'Landing Pad',
         'east': 'Forest',
-        'description': 'You can go North, South, East, West \n You hear nothing to the North \n You hear barking to the West \n A loud engine sound comes from the south \n Chanting and howling comes from the East \n'
+        'people': 'Karen',
+        'description': 'You can go North, South, East, West \n You hear nothing to the North \n You hear barking to the West \n A loud engine sound comes from the south \n Chanting and howling coming from the East \n Karen demands you feed her dog immediately else she\'ll be talking to your manager! \n'
     },
 
     # This is the landing Pad with directions, inspectables and description
     'Landing Pad': {
         'north': 'Garden',
         'inspect': 'Helicopter',
-        'description': 'You can go north \n You realize you have made a mistake and this is not the main character \n'
+        'people': 'Jason',
+        'description': 'You can go north \n The pilot says "Bring me some fuel and I can get us out of here!" \n'
     },
 
     # This is the Forest with directions and description
     'Forest': {
         'west': 'Garden',
         'east': 'Stream',
-        'description': 'You can go west or east \n Sounds of digging come from the West \n Sounds of a raging river come from the east \n'
+        'people': 'Larry',
+        'description': 'You can go west or east \n Sounds of digging come from the West \n Sounds of a raging river come from the east \n You see a half-naked man running after you, most likely rabbid, and quickly toss some bread in his direction \n'
     },
 
     # This is the Stream with directions and description
     'Stream': {
         'west': 'Forest',
-        'description': 'You can go west \n You hear a whisper on the wind, flying is dangerous! \n'
+        'item': 'treasure' + " and " + "blanket" + " but you can only have one.",
+        'people': 'Squirrels',
+        'description': 'You can go west \n You hear a whisper on the wind, flying is dangerous! \n You look down into the river basin and see something shimmering and shiny \n'
     },
 
     # This is the Bed Room with directions and description
-    'Bed Room': {
+    'Bedroom': {
         'west': 'Hall',
-        'description': 'You can go West \n You hear the sound of someone walking passed \n'
+        'item': 'candle',
+        'description': 'You can go West \n You hear the sound of someone walking passed \n You consider taking a nap, but are a bit hungry too \n',
     },
 
     # This is the Dining Room with directions and description
     'Dining Room': {
         'south': 'Hall',
         'north': 'Kitchen',
-        'description': 'You can go North or South \n You hear the sounds of cooking and clatter to the north \n You look to the south are still trying to find the bathroom \n'
+        'people': 'Will' + " and " + 'Keith' + " and " + 'Phoenix' + " and " + "Tara",
+        'description': 'You can go North or South \n You hear the sounds of cooking and clatter to the north \n You look to the south are still trying to find the bathroom \n You hear Phoenix talking about how she lost her blanket after being chased by some lunatic \n'
     },
 
     # This is the Kitchen with directions and description
     'Kitchen': {
         'south': 'Dining Room',
-        'description': 'You can go south \n You overhear someone talking about a secret bookshelf \n'
+        'item': 'bread',
+        'people': 'Bandon' + " and " + 'Carl',
+        'description': 'You can go south \n You overhear talk about a secret behind a bookshelf \n'
     },
 
     'Secret': {
         'east': 'Library',
         'item': 'script',
+        'people': 'Parrot',
         'description': 'You can go East \n You are wondering who allowed someone to bring a donut into the library \n'
     }
 }
@@ -197,17 +207,31 @@ while True:
             print(move[1] + ' - You inspected the item!')
 
     # If a player enters a room with a dog and a donut then they die
-    if 'inspect' in room[currentRoom] and 'dog' in room[currentRoom]['inspect'] and 'donut' in inventory:
-        print('You see a friendly Doberman, you reach out to pet it \n The Doberman smells your donut and attacks you \n YOU DIED! \n The doberman is not the main characte')
+    if 'people' in room[currentRoom] and 'Dog' in room[currentRoom]['people'] and 'donut' in inventory:
+        print('You see a friendly Doberman, you reach out to pet it \n The Doberman smells your donut and attacks you \n The doberman is not the main character \n YOU DIED! \n')
         break
 
-        # If a player enters the forest with fuel then they die
-    if 'inspect' in room[currentRoom] and 'dog' in room[currentRoom]['inspect'] and 'donut' in inventory:
-        print('You see a friendly Doberman, you reach out to pet it \n The Doberman smells your donut and attacks you \n YOU DIED! \n The doberman is not the main character, just hungry')
+        # If a player enters the landing pad with fuel then they die
+    if 'people' in room[currentRoom] and 'Jason' in room[currentRoom]['people'] and 'fuel canister' in inventory:
+        print('You hand Jason the fuel and hop into the chopper \n As you hop in you read the inscription Alta3 on the chopper \n As you fly away you see a missile coming straight at you and realize, Jason is Not the main character \n YOU DIED!')
         break
 
+        # Why would you jump in the river? This your first RPG?
+    if 'treasure' in inventory:
+        print('You look hard and realize there is a chest of solid gold in the river \n You quickly jump in after the treasure \n You realize this river is flowing very quickly and start to get pulled under \n Why would you jump in the river? This your first RPG? \n YOU DIED!')
+        break
+
+        # How do you survive a trip into the forest?
+    if 'people' in room[currentRoom] and 'Larry' in room[currentRoom]['people'] and 'bread' not in inventory:
+        print('You see a half-naked man running after you, certainly he must be lost! \n You realize he is rabbid and canabalistic \n Larry is NOT the main character \n YOU DIED!')
+        break
+
+        # If a player enters the library with a candle then they die
+    if 'inspect' in room[currentRoom] and 'table' in room[currentRoom]['inspect'] and 'candle' in inventory:
+        print('You decide to sit down and read the Twilight series books \n Your eye lids are feeling heavy and you find yourself nodding off \n You knock over your candle in your sleep and set the library on fire \n YOU DIED!')
+        break
 
 # Define how a player can win
-    if currentRoom == 'Secret' and 'donut' in inventory and 'script' in inventory:
-        print('You finally stopped to eat your donut and read the script and just realized... YOU are the main character \n You WIN!')
+    if currentRoom == 'Bedroom' and 'donut' in inventory and 'script' in inventory and 'blanket' in inventory:
+        print('You finally stopped to lay down, eat your donut and read the script and just realized... YOU, Pete, are the main character \n You WIN!')
         break
